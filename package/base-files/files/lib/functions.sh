openwrt/openwrt

base-files: add find_scsi_part function to find scsi device
This patch adds "find_scsi_part" to find partition on scsi device, such
as HDD and SSD. (partition name example: sda1, sdb2, etc...)

Signed-off-by: INAGAKI Hiroshi <musashino.open@gmail.com>

package/base-files/files/lib/functions.sh
@@ -353,6 +353,10 @@ find_mmc_part() {
 	find_blkdev_part_common "mmcblk" "p" $@
 }
 
+find_scsi_part() {
+	find_blkdev_part_common "sd" "" $@
+}
+
 group_add() {
 	local name="$1"
 	local gid="$2"

musashino-build/openwrt_old

package/base-files/files/lib/functions.sh
@@ -353,6 +353,10 @@ find_mmc_part() {
 	find_stor_part "mmcblk" "p" $@
 }
 
+find_scsi_part() {
+	find_stor_part "sd" "" $@
+}
+
 group_add() {
 	local name="$1"
 	local gid="$2"