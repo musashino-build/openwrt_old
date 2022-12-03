takotakot/openwrt

ath79: add support for NEC Aterm WR8175N

target/linux/ath79/image/lzma-loader/src/board.c
@@ -51,6 +51,7 @@ static inline void tlwr1043nd_init(void) {}
 #endif
 
 #if defined(CONFIG_BOARD_WG600HP) || \
+    defined(CONFIG_BOARD_WR8175N) || \
     defined(CONFIG_BOARD_WR8750N) || \
     defined(CONFIG_BOARD_WR9500N)
 static void nec_init(void)

musashino-build/openwrt_old

target/linux/ath79/image/lzma-loader/src/board.c
@@ -51,6 +51,7 @@ static inline void tlwr1043nd_init(void) {}
 #endif
 
 #if defined(CONFIG_BOARD_NEC_WG300HP) || \
+    defined(CONFIG_BOARD_NEC_WR8175N) || \
     defined(CONFIG_BOARD_NEC_WR8750N)
 static void nec_init(void)
 {