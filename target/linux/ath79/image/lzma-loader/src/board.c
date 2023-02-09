takotakot/openwrt

ath79: add support for NEC Aterm WR9500N

target/linux/ath79/image/lzma-loader/src/board.c
@@ -50,7 +50,8 @@ static void tlwr1043nd_init(void)
 static inline void tlwr1043nd_init(void) {}
 #endif
 
-#ifdef CONFIG_BOARD_WR8750N
+#if defined(CONFIG_BOARD_WR8750N) || \
+    defined(CONFIG_BOARD_WR9500N)
 static void nec_init(void)
 {
 	unsigned int reg = KSEG1ADDR(AR71XX_RESET_BASE);

musashino-build/openwrt_old

target/linux/ath79/image/lzma-loader/src/board.c
@@ -50,7 +50,8 @@ static void tlwr1043nd_init(void)
 static inline void tlwr1043nd_init(void) {}
 #endif
 
-#ifdef CONFIG_BOARD_NEC_WR8750N
+#if defined(CONFIG_BOARD_NEC_WR8750N) || \
+    defined(CONFIG_BOARD_NEC_WR9500N)
 static void nec_init(void)
 {
 	unsigned int reg = KSEG1ADDR(AR71XX_RESET_BASE);