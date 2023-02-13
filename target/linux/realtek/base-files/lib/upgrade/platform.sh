9743aab10963c49155462735d5ca0284c5911d8f
INAGAKI Hiroshi <musashino.open@gmail.com>
2023-02-13 22:40:02 +0900

    realtek: handle dual-boot on APRESIA ApresiaLightGS120GT-SS

--- target/linux/realtek/base-files/lib/upgrade/platform.sh
+++ target/linux/realtek/base-files/lib/upgrade/platform.sh
@@ -1,8 +1,8 @@
 PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
-RAMFS_COPY_BIN='fw_printenv fw_setenv'
-RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+RAMFS_COPY_BIN='fw_printenv fw_setenv fw_printsys'
+RAMFS_COPY_DATA='/etc/fw_env.config /etc/fw_sys.config /var/lock/fw_printenv.lock'
 
 # The TP-Link bootloader gets its flash layout from "bootargs".
 # Use this to our advantage, and:
@@ -19,6 +19,31 @@ tplink_sg2xxx_fix_mtdparts() {
 	echo -e "$args" | fw_setenv --script -
 }
 
+# Get the current active firmware partition by image index
+# got from system partition (u-boot-env2).
+#  $1: key name
+#  $2: index values of 1st and 2nd
+#      (format: <1st>:<2nd>, ex.: 1:2)
+#  $3: firmware partitions of 1st and 2nd
+#      (format: <1st>:<2nd>, ex.: firmware:firmware2, default: firmware:firmware2)
+sys_get_image_part() {
+	local key=$1
+	local val=$2 _val
+	local part=${3:-firmware:firmware2}
+
+	_val=$(fw_printsys -n "$key")
+	[ -z "$_val" ] &&
+		echo "failed to get value of $key" 1>&2 && return 1
+
+	case "$_val" in
+	"${val/:*/}") echo "${part/:*/}" && return ;;
+	"${val/*:/}") echo "${part/*:/}" && return ;;
+	esac
+
+	echo "invalid index number! $_val" 1>&2
+	return 1
+}
+
 platform_check_image() {
 	return 0
 }
@@ -30,6 +55,10 @@ platform_do_upgrade() {
 	tplink,sg2008p-v1|\
 	tplink,sg2210p-v3)
 		tplink_sg2xxx_fix_mtdparts
+		default_do_upgrade "$1"
+		;;
+	apresia,aplgs120gtss)
+		PART_NAME=$(sys_get_image_part Image_Id 1:2) || exit 1
 		default_do_upgrade "$1"
 		;;
 	*)
