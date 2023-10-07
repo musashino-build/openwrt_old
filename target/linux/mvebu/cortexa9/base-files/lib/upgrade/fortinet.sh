53c3bff16150edb8650e7b2dfe71725cea291851
INAGAKI Hiroshi <musashino.open@gmail.com>
2023-10-07 23:26:33 +0900

    mvebu: add dual-boot support for Fortinet FortiGate 50E

--- target/linux/mvebu/cortexa9/base-files/lib/upgrade/fortinet.sh
+++ target/linux/mvebu/cortexa9/base-files/lib/upgrade/fortinet.sh
@@ -1,54 +1,100 @@
 . /lib/functions.sh
 
 fortinet_fwinfo_blocks() {
-	local fwinfo_mtd="$(find_mtd_part firmware-info)"
-	local offset="$1"
-	local len="$2"
+	local fwinfo_mtd="$1"
+	local offset="$2"
+	local val="$3"
 	local blks
 
-	if [ -z "$fwinfo_mtd" ]; then
-		echo "WARN: MTD device \"firmware-info\" not found"
-		return 1
-	fi
+	blks=$((val / 0x200))
+	[ $((val % 0x200)) -gt 0 ] && blks=$((blks + 1))
+	blks=$(printf "%08x" $blks)
+	printf "fwinfo: offset-> 0x%x, blocks-> 0x%s (val: 0x%08x)\n" \
+		$offset $blks $val
 
-	blks=$((len / 0x200))
-	[ $((len % 0x200)) -gt 0 ] && blks=$((blks + 1))
-	blks=$(printf "%04x" $blks)
-	printf "fwinfo: offset-> 0x%x, blocks-> 0x%s (len: 0x%08x)\n" \
-		$offset $blks $len
-
-	printf "\x${blks:2:2}\x${blks:0:2}" | \
-		dd bs=2 count=1 seek=$((offset / 2)) conv=notrunc of=${fwinfo_mtd}
+	printf "\x${blks:6:2}\x${blks:4:2}\x${blks:2:2}\x${blks:0:2}" | \
+		dd bs=4 count=1 seek=$((offset / 4)) conv=notrunc of=${fwinfo_mtd}
 }
 
 fortinet_do_upgrade() {
+	local fwinfo_mtd="$(find_mtd_part firmware-info)"
 	local board_dir="$(tar tf "$1" | grep -m 1 '^sysupgrade-.*/$')"
-	local kern_mtd="$(find_mtd_part kernel)"
-	local root_mtd="$(find_mtd_part rootfs)"
+	local kern_len_ofs root_len_ofs
 	local kern_len root_len
+	local part_index
 
 	board_dir="${board_dir%/}"
 
-	if [ -z "$kern_mtd" ] || [ -z "$root_mtd" ]; then
-		echo "ERROR: MTD device \"kernel\" or \"rootfs\" not found"
-		umount -a
-		reboot -f
+	if [ -z "$fwinfo_mtd" ]; then
+		v "ERROR: MTD device \"firmware-info\" not found"
+		return 1
 	fi
 
-	kern_len=$( (tar xOf "$1" "$board_dir/kernel" | wc -c) 2> /dev/null)
-	root_len=$( (tar xOf "$1" "$board_dir/root" | wc -c) 2> /dev/null)
+	part_index=$(hexdump -n 1 -s $((0x170)) -e '"%d"' $fwinfo_mtd)
+	case $part_index in
+	0)
+		kern_len_ofs=0x184
+		root_len_ofs=0x18c
+		PART_NAME="firmware"
+		;;
+	1)
+		kern_len_ofs=0x194
+		root_len_ofs=0x19c
+		PART_NAME="firmware2"
+		;;
+	*)
+		return 1
+	esac
+
+	tar xOf "$1" "$board_dir/kernel" > /tmp/fw_kernel.bin 2>/dev/null
+	tar xOf "$1" "$board_dir/root" > /tmp/fw_root.bin 2>/dev/null
+	# pad kernel
+	dd if=/tmp/fw_kernel.bin of=/tmp/fw_kernel.pad \
+		bs=$((0x10000)) conv=sync 2>/dev/null
+	# concat kernel and rootfs
+	cat /tmp/fw_kernel.pad /tmp/fw_root.bin > /tmp/fw.bin
+
+	kern_len=$(wc -c /tmp/fw_kernel.pad | cut -d' ' -f1)
+	root_len=$(wc -c /tmp/fw_root.bin | cut -d' ' -f1)
 
 	if [ -z "$kern_len" ] || [ -z "$root_len" ]; then
-		echo "ERROR: failed to get length of new kernel or rootfs"
-		umount -a
-		reboot -f
+		v "ERROR: failed to get length of new kernel or rootfs"
+		return 1
 	fi
 
-	fortinet_fwinfo_blocks "0x184" "$kern_len"
-	fortinet_fwinfo_blocks "0x18c" "$root_len"
+	fortinet_fwinfo_blocks "$fwinfo_mtd" "$kern_len_ofs" "$kern_len" || return 1
+	fortinet_fwinfo_blocks "$fwinfo_mtd" "$root_len_ofs" "$root_len" || return 1
 
-	tar xOf "$1" "$board_dir/kernel" | \
-		mtd write - "kernel"
-	tar xOf "$1" "$board_dir/root" | \
-		mtd ${UPGRADE_BACKUP:+-j "${UPGRADE_BACKUP}"} write - "rootfs"
+	default_do_upgrade "/tmp/fw.bin"
+}
+
+fortinet_set_active_part() {
+	local fwinfo_mtd="$(find_mtd_part firmware-info)"
+	local part_name="$1"
+	local index
+	local imgname_seek
+
+	if [ -z "$fwinfo_mtd" ]; then
+		echo "ERROR: MTD device \"firmware-info\" not found"
+		return 1
+	fi
+
+	case "$part_name" in
+	firmware)
+		index=0
+		imgname_seek=1
+		;;
+	firmware2)
+		index=1
+		imgname_seek=3
+		;;
+	*)
+		echo "invalid partition name specified"
+		return 1
+		;;
+	esac
+
+	echo -n "\x$index" | \
+		dd of=$fwinfo_mtd bs=1 seek=$((0x170)) count=1 conv=notrunc \
+			2>/dev/null
 }
